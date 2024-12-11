142665f9b283d3745a0d93650241a17d78efa77d
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2024-12-11 18:33:07 +0330

    fix our error

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,14 +1,19 @@
-const Card = ({ food }) => {
+const Card = ({ food }:any) => {
   return (
-    <div
-      className="flex flex-col p-6 border-solid border-1 border-[grey] rounded-md bg-white shadow-lg h-[350px]"
-    >
-      <img className="h-44 w-full rounded-md mb-2" src={(`${food.image}`)} />
-      <h2 className="font-bold text-lg mb-2">{food.name}</h2>
-      <p className="font-normal text-base text-slate-700 mb-2">{food.explanation}</p>
-      <span className="font-semibold text-base text-orange-500 mb-2">
-        {food.prize}
-      </span>
+    <div className="flex flex-col p-6 border-solid border-1 border-[grey] rounded-md bg-white shadow-lg h-[350px]">
+      <img
+        className="h-48 w-11/12 rounded-md mb-2 mx-auto"
+        src={`${food.image}`}
+      />
+      <h2 className="font-bold text-lg mb-2 mx-auto">{food.name}</h2>
+      <p className="font-normal text-base text-slate-700 mb-2 mx-auto">
+        {food.explanation}
+      </p>
+      <div className="mr-8">
+        <span className="font-semibold text-base text-orange-500 mb-2">
+          {food.prize}
+        </span>
+      </div>
     </div>
   );
 };
