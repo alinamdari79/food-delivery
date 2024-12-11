a2c2e43e6713e73b951b02f9ba42c2bdb82990f0
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2024-12-11 19:11:41 +0330

    add counter buttons to cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,6 +1,10 @@
-const Card = ({ food }:any) => {
+import { useState } from 'react'
+
+const Card = ({ food }: any) => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="flex flex-col p-6 border-solid border-1 border-[grey] rounded-md bg-white shadow-lg h-[350px]">
+    <div className="flex flex-col p-4 border-solid border-1 border-[grey] rounded-md bg-white shadow-lg h-[380px]">
       <img
         className="h-48 w-11/12 rounded-md mb-2 mx-auto"
         src={`${food.image}`}
@@ -9,10 +13,15 @@ const Card = ({ food }:any) => {
       <p className="font-normal text-base text-slate-700 mb-2 mx-auto">
         {food.explanation}
       </p>
-      <div className="mr-8">
+      <div className="mr-8 flex flex-row gap-24 mt-2">
         <span className="font-semibold text-base text-orange-500 mb-2">
           {food.prize}
         </span>
+        <div className="border-2 border-zinc-500 w-[100px] rounded-md flex justify-between">
+          <button onClick={() => setCount((count) => count - 1)} className="bg-slate-500 h-full w-8">-</button>
+          <span>{count}</span>
+          <button onClick={() => setCount((count) => count + 1)} className="bg-orange-500 h-full w-8">+</button>
+        </div>
       </div>
     </div>
   );
